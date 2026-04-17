4c02fc4721a6e706f21ebc81542df0bdb8287603
Ningsihsyam <ningsihsyam3@gmail.com>
2026-04-17 18:52:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from download_data import download_stock_data
 from visualize import buat_grafik_tren
+from notify import cek_sinyal_dan_notifikasi # Import baru
 
 def main():
     print("--- Memulai Pipeline Otomasi ---")
@@ -10,6 +11,10 @@ def main():
     # 2. Jalankan Visualisasi
     if df_matang is not None:
         buat_grafik_tren(df_matang)
+        # Kirim notifikasi setelah grafik jadi
+        print("Mengirim notifikasi ke Telegram...")
+        cek_sinyal_dan_notifikasi()
+
         print("--- Pipeline Sukses! ---")
 
 if __name__ == "__main__":
